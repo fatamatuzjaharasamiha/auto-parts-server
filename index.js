41edfc7969f1c8c9e82f373906e8e422b587600a
fatamatuzjaharasamiha <samirajahara843@gmail.com>
2022-05-26 13:57:33 +0600

    set uri

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://car_admin:<password>@cluster0.htbbs.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.htbbs.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
 
 app.get('/', (req, res) => {
     res.send('Running auto parts server')
