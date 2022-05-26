b463145b775763fb7eae14febaf79481218f78e3
fatamatuzjaharasamiha <samirajahara843@gmail.com>
2022-05-26 13:43:33 +0600

    set dbuser and dbpass

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = "mongodb+srv://car_admin:<password>@cluster0.htbbs.mongodb.net/?retryWrites=true&w=majority";
 
 app.get('/', (req, res) => {
     res.send('Running auto parts server')
