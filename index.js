d06709a9ac9b48acf0512ce363081d967ed0a3b5
fatamatuzjaharasamiha <samirajahara843@gmail.com>
2022-05-27 02:37:48 +0600

    get individual product

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -12,6 +12,37 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.htbbs.mongodb.net/?retryWrites=true&w=majority`;
 
 console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('database connected');
+        const productCollection = client.db('auto_parts').collection('products');
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products)
+        })
+
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query)
+            res.send(product);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Running auto parts server')
