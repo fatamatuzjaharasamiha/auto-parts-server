5efa09f3b2ae1c9ba12c00568ac09ec881521963
fatamatuzjaharasamiha <samirajahara843@gmail.com>
2022-05-27 15:45:16 +0600

    JWT api implemented

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
+const jwt = require('jsonwebtoken');
 
 //middleware
 app.use(cors());
@@ -20,6 +21,23 @@ async function run() {
         console.log('database connected');
         const productCollection = client.db('auto_parts').collection('products');
         const orderCollection = client.db('auto_parts').collection('orders')
+        const userCollection = client.db('auto_parts').collection('users')
+
+
+
+        // put user to db
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email
+            const user = req.body
+            const filter = { email: email }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options)
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+            res.send({ result, token })
+        })
 
         app.get('/product', async (req, res) => {
             const products = await productCollection.find().toArray()
@@ -33,7 +51,7 @@ async function run() {
             res.send(product);
         })
 
-        // Put order in order collection
+        
 
         // put order in orders collection
         app.put('/order', async (req, res) => {
