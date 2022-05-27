e55fd026d3c7fac4dc60d38e7033b6da08937996
fatamatuzjaharasamiha <samirajahara843@gmail.com>
2022-05-27 14:10:26 +0600

    Purchase api implemented

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.htbbs.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -20,11 +19,10 @@ async function run() {
         await client.connect();
         console.log('database connected');
         const productCollection = client.db('auto_parts').collection('products');
+        const orderCollection = client.db('auto_parts').collection('orders')
 
         app.get('/product', async (req, res) => {
-            const query = {};
-            const cursor = productCollection.find(query);
-            const products = await cursor.toArray();
+            const products = await productCollection.find().toArray()
             res.send(products)
         })
 
@@ -33,6 +31,52 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const product = await productCollection.findOne(query)
             res.send(product);
+        })
+
+        // Put order in order collection
+
+        // put order in orders collection
+        app.put('/order', async (req, res) => {
+           
+            const order = req.body
+            const filter = {
+                email: order.email,
+                toolId: order.toolId
+            }
+            const query = { _id: ObjectId(order.toolId) }
+            const options = { upsert: true };
+            const exist = await orderCollection.findOne(filter)
+            if (exist) {
+                const updatedDoc = {
+                    $set: {
+                        address: order.address,
+                        phone: order.phone,
+                        quantity: exist.quantity + order.quantity,
+                        price: exist.price + order.price
+                    }
+                }
+                const tool = await productCollection.findOne(query)
+                const updateTool = {
+                    $set: {
+                        quantity: tool.quantity - order.quantity
+                    }
+                }
+                const updatedTool = await productCollection.updateOne(query, updateTool, options)
+                const updatedOrder = await orderCollection.updateOne(filter, updatedDoc, options)
+                return res.send({ updatedOrder, updatedTool })
+            }
+
+            else {
+                const tool = await productCollection.findOne(query)
+                const updateTool = {
+                    $set: {
+                        quantity: tool.quantity - order.quantity
+                    }
+                }
+                const updatedTool = await productCollection.updateOne(query, updateTool, options)
+                const result = await orderCollection.insertOne(order)
+                return res.send({ result, updatedTool })
+            }
         })
     }
     finally {
